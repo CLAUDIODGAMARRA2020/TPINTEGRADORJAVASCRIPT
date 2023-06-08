430471a4320dde10151bb0334ec7aa441338bdbb
jorge <test@fakemail.com>
2023-06-08 20:52:20 -0300

    Modificacion de TP Integrador

---
+++ calculos.js
@@ -0,0 +1,14 @@
+
+function resumen(){
+
+/*const precioTicket =200;
+
+function resumen(){
+    
+    var cantidadTicket =document.getElementById('inputCantidad'), value;
+    var descuento = document.getElementById('inputCategoria'), value;
+
+    var precioTicket = cantidadTicket *(precioTicket-((precioTicket*descuento)/ 100));
+
+    document.getElementById('compraTotal').value='Total a Pagar: $' + precio;
+}
